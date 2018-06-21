7ec73fe91486b719b93483e9f912453c97199cd5
Lincoln Pahwa <lincolnpahwa@Lincolns-MBP.fios-router.home>
2018-06-21 07:36:39 -0400

    Style updates, Image name formatting

---
+++ app/helpers/image-name.js
@@ -0,0 +1,14 @@
+import { helper } from '@ember/component/helper';
+import { capitalize } from '@ember/string';
+
+export function imageName(params /*, hash*/ ) {
+  const [name] = params;
+  
+  if (name) {
+  	return name.split(' ').map(capitalize).join(' ').split('-').map(capitalize).join(' ');
+  } else {
+  	return params;
+  }
+}
+
+export default helper(imageName);
